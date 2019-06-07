cf90b66c00887e035862a278a9530cb03a82c624
Stephane Frenot <stephane.frenot@insa-lyon.fr>
2019-06-07 15:16:55 +0200

    Migration de code

---
+++ seedbeat-SimulFutur/config/config.go
@@ -0,0 +1,16 @@
+// Config is put into a different package to prevent cyclic imports in case
+// it is needed in several locations
+
+package config
+
+import "time"
+
+type Config struct {
+	Period time.Duration `config:"period"`
+	Seed string `config:"seed"`
+}
+
+var DefaultConfig = Config{
+	Period: 1 * time.Second,
+  Seed: "seed.bitcoin.sipa.be",
+}
